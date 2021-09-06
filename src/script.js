3b356c55687dee219000a8de8c0331cf80712645
dekel <dekel99s@gmail.com>
2021-09-06 16:46:26 +0300

    static work page & bug fixes

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ import './style.css'
 // Show fps config
 // var stats = new Stats();
 // stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
+// document.body.appendChild( stats.dom );
 
 let spaceship
 let sun3
